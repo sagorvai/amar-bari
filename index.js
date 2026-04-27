53009ad216beb7971653f31cd09abeb7cf2e6804
sagorvai <82975730+sagorvai@users.noreply.github.com>
2026-04-28 03:17:42 +0600

    Update index.js

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ async function initMap() {
             const data = doc.data();
             if (data.location && data.location.lat && data.location.lng) {
                 const marker = L.marker([data.location.lat, data.location.lng], {
-                    icon: createCustomMarker(data.category, data.Type || 'বাড়ি''জমি''প্লট''ফ্লাট''অফিস''দোকান')
+                    icon: createCustomMarker(data.category, data.Type || )
                 }).addTo(map);
 
                 marker.on('click', () => {
