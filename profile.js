c2ecfe631c5c909e132e658a04c91f738dd5fb2a
sagorvai <82975730+sagorvai@users.noreply.github.com>
2026-06-14 11:50:09 +0600

    Implement tab navigation for profile editing
    Added a function to handle tab navigation in the profile edit section.

--- profile.js
+++ profile.js
@@ -37,6 +37,17 @@ document.addEventListener('DOMContentLoaded', function() {
     const avatarPreview = document.getElementById('edit-avatar-preview');
     const fileInput = document.getElementById('edit-profile-picture');
 
+    
+      function openTab(evt, tabName) {
+            var i, tabcontent, tablinks;
+            tabcontent = document.getElementsByClassName("tab-content");
+            for (i = 0; i < tabcontent.length; i++) { tabcontent[i].classList.remove("active"); }
+            tablinks = document.getElementsByClassName("tab-btn");
+            for (i = 0; i < tablinks.length; i++) { tablinks[i].classList.remove("active"); }
+            document.getElementById(tabName).classList.add("active");
+            evt.currentTarget.classList.add("active");
+      }
+    
     // ১. ফায়ারবেস অথেনটিকেশন চেক
     auth.onAuthStateChanged(async (user) => {
         if (user) {
